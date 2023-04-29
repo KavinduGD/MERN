a74d07469756a23dade3cf7251f31dd927533aa8
it21038914 <it21038914@my.sliit.lk>
2023-04-29 22:22:40 +0530

    another

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,25 +1,35 @@
+const asyncHandler = require("express-async-handler");
+
 //get all goals
 //routes  GET /api/goals
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
 //create a goal
 //routes  POST /api/goals
-const createGoal = (req, res) => {
-  res.status(200).json({ message: "Create a goal" });
-};
+const createGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please enter a text feild");
+  }
+  console.log("sdf");
+  res.status(200).json({ message: "create a goal" });
+});
 
 //update a goal
 //routes  PUT /api/goals/:id
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update a goal ${req.params.id}` });
-};
+});
 
 //delete a goal
 //routes  DELETE /api/goals/:id
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete a goal ${req.params.id}` });
-};
+});
 
 module.exports = { getGoals, createGoal, updateGoals, deleteGoals };
+
+//async wait handle karanna api try catch block eka use karanna one
+//ehema karanna eka venuwata thama async-handler module eka download karanne
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,20 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const router = require("./routes/goalRoutes");
-
+const { errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 
+//express.json() is a middleware function that parses incoming requests with JSON payloads. It parses the JSON payload of the incoming request and attaches it to the req object as a body property.
+//body eken ena json ewata access krnna puluwn karanawa
+app.use(express.json());
+
+//express.urlencoded() is a middleware function that parses incoming requests with urlencoded payloads. It parses the urlencoded payload of the incoming request and attaches it to the req object as a body property.
+//body eken ena urlencoded ewata access krnna puluwn karanawa
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", router);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
