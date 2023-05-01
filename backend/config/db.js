26d694570822fee304b52e47cbace1016b22462a
it21038914 <it21038914@my.sliit.lk>
2023-05-02 01:45:26 +0530

    just comming

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,6 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(error);
